1c8ae04b579c0dfa5f621a021628eba38649fba4
Viktoryia Danko <viktoryia.danko@LeverX.local>
2022-08-12 23:02:56 +0300

    delete burger one

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-const menuButton = document.querySelector(".more-btn");
-let menuOpen = false;
-menuButton.addEventListener("click", () => {
-  if (!menuOpen) {
-    menuButton.classList.add("open");
-    menuOpen = true;
-  } else {
-    menuButton.classList.remove("open");
-    menuOpen = false;
-  }
-});
 
-const buttonOne = document.querySelector("menu-btn-one");
-let btnOpen = false;
-buttonOne.addEventListener("click", () => {
-  if (!btnOpen) {
-    buttonOne.classList.add("open");
-    btnOpen = true;
-  } else {
-    buttonOne.classList.remove("open");
-    btnOpen = false;
-  }
-});
+
+
