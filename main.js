eb64cf921ee10f765500e33116a80f79958963e7
Viktoryia Danko <viktoryia.danko@LeverX.local>
2022-08-12 23:38:49 +0300

    add js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const menuButton = document.querySelector(".menu-btn-one");
 
+const lineOne = document.querySelector(".line-one");
+const lineTwo = document.querySelector(".line-two");
+const lineThree = document.querySelector(".line-three");
 
-
+menuButton.addEventListener("click", function () {
+  lineOne.classList.toggle("open");
+  lineTwo.classList.toggle("open");
+  lineThree.classList.toggle("open");
+});
