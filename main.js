a59c24ec1b43feb1b181d846d75f1d6efd74905d
Viktoryia Danko <viktoryia.danko@LeverX.local>
2022-08-13 13:05:04 +0300

    add animation to box menu

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const menuButton = document.querySelector(".menu-btn-one");
-
 const lineOne = document.querySelector(".line-one");
 const lineTwo = document.querySelector(".line-two");
 const lineThree = document.querySelector(".line-three");
+const boxMenu = document.querySelector(".menu-box");
+
+console.log(boxMenu);
 
 menuButton.addEventListener("click", function () {
   lineOne.classList.toggle("open");
   lineTwo.classList.toggle("open");
   lineThree.classList.toggle("open");
+  boxMenu.classList.toggle("open");
 });
