62b591a92acbe8b8b84bd7dff84a333ac853938c
Viktoryia Danko <viktoryia.danko@LeverX.local>
2022-08-14 11:30:29 +0300

    create modal window

--- main.js
+++ main.js
@@ -3,12 +3,21 @@ const lineOne = document.querySelector(".line-one");
 const lineTwo = document.querySelector(".line-two");
 const lineThree = document.querySelector(".line-three");
 const boxMenu = document.querySelector(".menu-box");
-
-console.log(boxMenu);
+const button = document.querySelector(".modal-btn");
+const modalClosed = document.querySelector(".login-btn");
+const modal = document.querySelector(".modal-block");
 
 menuButton.addEventListener("click", function () {
   lineOne.classList.toggle("open");
   lineTwo.classList.toggle("open");
   lineThree.classList.toggle("open");
   boxMenu.classList.toggle("open");
+});
+
+button.addEventListener("click", function () {
+  modal.classList.toggle("open");
+});
+
+modalClosed.addEventListener("click", function () {
+  modal.classList.toggle("close");
 });
